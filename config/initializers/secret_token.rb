1ec185d12bb133d9743e33ad256bdb62afa2671d
Theresa Solak <yarfreese@gmail.com>
2013-04-30 17:00:49 -0700

    after: rails new busy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Busy::Application.config.secret_token = '50d7429fe942db2bdecca3eb5fb09c867266e680b4efaff527d7690d6cec2f063af3811eb333de5192cb05b130a9e367542851792288dcf178ae12e7a422ebe6'
